5b28ebff1feb1bd7f9ae8cf241601eba50a79f5f
Sebastien Saunier <seb@saunier.me>
2015-02-09 11:25:25 +0100

    Get rid of

--- models/restaurant_spec.rb
+++ models/restaurant_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  it "should have a name" do
+  it "has a name" do
     restaurant = Restaurant.new(name: "La Tour d'Argent")
     expect(restaurant.name).to eq("La Tour d'Argent")
   end
 
-  it "should have an address" do
-    restaurant = Restaurant.new(address: "La Tour d'Argent")
+  it "has an address" do
+    restaurant = Restaurant.new(address: "15 Quai de la Tournelle, 75005 Paris")
     expect(restaurant.address).to eq("15 Quai de la Tournelle, 75005 Paris")
   end
 
-  it "should have a phone number" do
+  it "has a phone number" do
     restaurant = Restaurant.new(phone_number: "01 43 54 23 31")
     expect(restaurant.phone_number).to eq("01 43 54 23 31")
   end
 
-  it "should have a category" do
+  it "has a category" do
     restaurant = Restaurant.new(category: "french")
     expect(restaurant.category).to eq("french")
   end
