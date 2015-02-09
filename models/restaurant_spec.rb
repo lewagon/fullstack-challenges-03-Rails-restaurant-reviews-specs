66a763171091aca0368ecd53b9ad0dbba0799514
Sebastien Saunier <seb@saunier.me>
2015-02-09 11:24:51 +0100

    Testing all restaurant attributes

--- models/restaurant_spec.rb
+++ models/restaurant_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Restaurant, :type => :model do
     restaurant = Restaurant.new(name: "La Tour d'Argent")
     expect(restaurant.name).to eq("La Tour d'Argent")
   end
+
+  it "should have an address" do
+    restaurant = Restaurant.new(address: "La Tour d'Argent")
+    expect(restaurant.address).to eq("15 Quai de la Tournelle, 75005 Paris")
+  end
+
+  it "should have a phone number" do
+    restaurant = Restaurant.new(phone_number: "01 43 54 23 31")
+    expect(restaurant.phone_number).to eq("01 43 54 23 31")
+  end
+
+  it "should have a category" do
+    restaurant = Restaurant.new(category: "french")
+    expect(restaurant.category).to eq("french")
+  end
 end
