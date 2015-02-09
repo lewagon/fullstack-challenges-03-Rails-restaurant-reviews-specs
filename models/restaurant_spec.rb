7ede85d2ac4fd6aeca46ed9518568076aea4042c
Sebastien Saunier <seb@saunier.me>
2015-02-09 11:28:39 +0100

    Test name cannot be blank on Restaurant

--- models/restaurant_spec.rb
+++ models/restaurant_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
+  let(:valid_attributes) do
+    {
+      name: "La Tour d'Argent",
+      address: "15 Quai de la Tournelle, 75005 Paris",
+      phone_number: "01 43 54 23 31",
+      category: "french"
+    }
+  end
+
   it "has a name" do
     restaurant = Restaurant.new(name: "La Tour d'Argent")
     expect(restaurant.name).to eq("La Tour d'Argent")
@@ -19,5 +28,12 @@ RSpec.describe Restaurant, :type => :model do
   it "has a category" do
     restaurant = Restaurant.new(category: "french")
     expect(restaurant.category).to eq("french")
+  end
+
+  its "name cannot be blank" do
+    attributes = valid_attributes
+    attributes.delete(:name)
+    restaurant = Restaurant.new(attributes)
+    expect(restaurant).not_to be_valid
   end
 end
